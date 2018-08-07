700da43e80ea3b6c7086925cbebcc8f784f2d0d0
Nicanor Diaz <diaz.nicanor123@gmail.com>
2018-08-07 08:27:35 -0300

    Agrego JFrame

--- src/Fixture/Fixture.java
+++ src/Fixture/Fixture.java
@@ -1,9 +1,21 @@
 package Fixture;
 
+import java.awt.Color;
+import javax.swing.BorderFactory;
+import javax.swing.JFrame;
+import javax.swing.border.Border;
+
 public class Fixture {
 
     public static void main(String[] args) {
+       JFrame frame = new JFrame("Fase de Grupos");
+       frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+       frame.setSize(500,500);
+       frame.setLayout(null);
+       frame.setResizable(false);
+    
+       Border borde = BorderFactory.createLineBorder(Color.BLACK);
        
-    }
+    }  
     
 }
